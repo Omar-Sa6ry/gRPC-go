227b0ce0a96464ddaf410daab817770d9687b15c
Omar <omarahmedsabry05@gmail.com>
2026-03-01 06:17:46 +0200

    feat: Implement client and server for GreetManyTimes server-side streaming RPC.

---
+++ greet/client/greet_many_times.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	"context"
+	"io"
+	"log"
+
+	pb "github.com/Omar-Sa6ry/grpc-go/greet/proto"
+)
+
+func doGreetManyTimes(client pb.GreetServiceClient) {
+	log.Println("doGreetManyTimes was invoked")
+
+	req := &pb.GreetRequest{
+		FirstName: "Omar",
+	}
+
+	stream, err := client.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes RPC: %v", err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading stream: %v", err)
+		}
+
+		log.Printf("Response from GreetManyTimes: %v", res.Result)
+	}
+}
--- greet/server/greet_many_times.go
+++ greet/server/greet_many_times.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	pb "github.com/Omar-Sa6ry/grpc-go/greet/proto"
 )
+
 func (s *Server) GreetManyTimes(in *pb.GreetRequest, stream pb.GreetService_GreetManyTimesServer) error {
-	log.Println("GreetManyTimes function was invoked with %v\n", in)
+	log.Printf("GreetManyTimes function was invoked with %v\n", in)
 
 	for i := 0; i < 10; i++ {
 		response := fmt.Sprintf("Hello %s, number %d", in.FirstName, i)
@@ -12,7 +16,7 @@ func (s *Server) GreetManyTimes(in *pb.GreetRequest, stream pb.GreetService_Gree
 		stream.Send(&pb.GreetResponse{
 			Result: response,
 		})
-
-		return nil
 	}
+
+	return nil
 }
